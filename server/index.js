1af4c1fc8bce23bcdf33a8ec72cce64524641295
Nouman Ejaz <noumanejaz@Noumans-MacBook-Pro.local>
2024-07-16 22:01:12 +0500

    final commit from mac

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("Database Connected"))
+.catch((error) => console.error("Connection failed ", error));
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.use('/', require('./routes/authRoute'))
+app.use('/', require('./routes/authRoute'));
 
 const PORT = 8000;
-app.listen(PORT, () => console.log(`Server is running on port htps://localhost:${8000}`))
+app.listen(PORT, () => console.log(`Server is running on port http://localhost:${PORT}`));
